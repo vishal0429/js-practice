af62d5ac92b348b23b26e15a1718407aee108c9c
Vishal Gupta <vishal.gupta@nimapinfotech.com>
2025-07-28 10:27:27 +0530

    new

--- js-practice.js
+++ js-practice.js
@@ -1,13 +1,33 @@
-var maxSum = function(nums) {
-   if(!nums.length) return;
-    let uniquie = [...new Set(nums)];
-    
-    let positive = uniquie.filter(num => num > 0);
-    
-    if(positive.length > 0){
-        return positive.reduce((sum, val)=> sum + val,0)
+// const sumExceptSelf = (arr) => {
+//   let result = [];
+//   let sum = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   for (let i = 0; i < arr.length; i++) {
+//     result[i] = sum - arr[i];
+//   }
+//   return result;
+// };
+
+// const arr = [1, 2, 3, 4];
+// const result = sumExceptSelf(arr);
+// console.log(result);
+
+const secondLargest = (arr) => {
+  let largestNumber = 0;
+  let secondLarget = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= largestNumber) {
+      secondLarget = largestNumber;
+      largestNumber = arr[i];
     }
-    else{
-        return Math.max(...uniquie);
-    }
+    else if(arr[i] > secondLargest &&  ){
+  }
 };
+
+const arr = [1, 2, 3, 4];
+const result = secondLargest(arr);
+console.log(result);
